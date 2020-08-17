5f14378db2f59d406c02dcc3ec44ed953305c9c6
Tej Charfi <charfi@usc.edu>
2020-08-17 15:24:05 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -36,7 +36,8 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
-  matrix [row][col]
+  
+  matrix[row][col]
 end
 
 def matrix_update(matrix, row, column, new_value)
